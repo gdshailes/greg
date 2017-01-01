8cad10428973b57943d907fd827cf6f013b9fcb8
gdshailes <greg.shailes@gmail.com>
2017-01-01 21:35:00 +0000

    Write JPEG to database

--- app/controllers/boriscam_controller.rb
+++ app/controllers/boriscam_controller.rb
@@ -12,35 +12,14 @@ class BoriscamController < ActionController::Base
 
   def upload
 
-    uploaded_io = params[:picture]
-
-    dest_path = Rails.root.join('app', 'assets', 'images')
-    new_pic = dest_path.join('boris_001.jpg')
-
-
-    # Delete 20
-    File.delete(dest_path.join('boris_020.jpg')) if File.exist?(dest_path.join('boris_020.jpg'))
-
-    # Rename 1-19 to 2-20
-    i = 20
-    while i > 1 do
-      i -= 1
-
-      rename_from = dest_path.join('boris_' + ('000' + i.to_s).last(3) + '.jpg')
-
-      if File.exist?(rename_from)
-        rename_to = dest_path.join('boris_' + ('000' + (i + 1).to_s).last(3) + '.jpg')
-        FileUtils.mv(rename_from, rename_to)
+    # build a photo and pass it into a block to set other attributes
+    @image = Image.new(params[:picture]) do |t|
+      if params[:picture][:data]
+        t.data = params[:picture][:data].read
       end
-
     end
 
-    File.open(new_pic, 'wb') do |file|
-
-      # Save new pic as 1
-      file.write(uploaded_io.read)
-
-    end
+    @image.Save
 
   end
 
---
+++ db/migrate/20170101210016_create_images.rb
@@ -0,0 +1,8 @@
+class CreateImage < ActiveRecord::Migration[5.0]
+  def change
+    create_table :image do |t|
+      t.integer :number
+      t.binary :image
+    end
+  end
+end
