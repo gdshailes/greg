c504106c39fa1ce04916f0ed39c33fdac7cc0203
gdshailes <greg.shailes@gmail.com>
2017-01-09 17:47:50 +0000

    Removed check for filetype

--- app/controllers/boriscam_controller.rb
+++ app/controllers/boriscam_controller.rb
@@ -18,14 +18,13 @@ class BoriscamController < ActionController::Base
     @image = Image.new
     @image.uploaded_file params['datafile']
 
-    if @image.filetype != "image/jpeg"
-      render nothing: true, status: 415 # Unsupported media type
-    else
+##    if @image.filetype != "image/jpeg"
+##      render nothing: true, status: 415 # Unsupported media type
+##    else
       @image.save
       Image.order('created_at desc').offset(20).destroy_all
-      # TODO: Delete oldest files if there are more than twenty files.
       render nothing: true, status: 200 # okay
-    end
+##    end
 
   end
 
