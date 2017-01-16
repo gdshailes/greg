362025cf69731a0f277787cb0a6281dfc8916390
gdshailes <greg.shailes@gmail.com>
2017-01-16 10:18:45 +0000

    Added generic index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
     root 'greg#index'
 
+    get 'greghome', to: 'greg#greghome'
     get 'boriscam', to: 'boriscam#index'
-
     post 'boriscam/upload', to: 'boriscam#upload'
 
 end
