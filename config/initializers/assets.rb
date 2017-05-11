515aa5314d144f320352ca5c9e228ffd858fe211
Greg Shailes <greg.shailes@gmail.com>
2017-05-11 09:34:57 +0100

    Update Bob the Fourth's address

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( borisCam.js )
-Rails.application.config.assets.precompile += %w( boriscam.css )
+
+
+Rails.application.config.assets.precompile += %w( ride_log.js )
+Rails.application.config.assets.precompile += %w( ride_log.css )
 
 Rails.application.config.assets.precompile += %w( jquery_ujs.js )
--- test/models/api_subscription_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApiSubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
