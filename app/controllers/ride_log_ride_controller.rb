c60b20fb82d956820cef4e279e90695cdb0714d9
Greg Shailes <greg.shailes@gmail.com>
2017-05-11 09:34:57 +0100

    Update Bob the Fourth's address

--- app/controllers/ride_log_ride_controller.rb
+++ app/controllers/ride_log_ride_controller.rb
@@ -17,19 +17,6 @@ class RideLogRideController < ActionController::Base
     ride = RideLogRide.new(name: p[:name], date: p[:date])
     ride.ride_log_bike_id = p[:bike_id]
     ride.save
-
-    # Notify any subscribers
-    ApiSubscription.where(event: :new_ride_created).each do |sub|
-      # Post the subscriber details of this new ride
-      response = HTTParty.post(
-        sub.target_url,
-        :body => {:date => ride.date,
-                  :name => ride.name,
-                }.to_json,
-        :headers => {'Content-Type' => 'application/json'}
-      )
-    end
-
     redirect_to action: :index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,9 @@ Rails.application.routes.draw do
 
     get 'ride_log', to: 'ride_log#index'
 
-    get 'api/new_ride', to: 'api/new_ride#index'
-    post 'api/new_ride', to: 'api/new_ride#new_ride'
-
     resources :ride_log_bike
     resources :ride_log_ride
 
-    get '/api/ping', to: 'api/ping#index'
-    get '/api/ride_log_ride', to: 'api/ride_log_ride#index'
-    get '/api/ride_log_ride/create', to: 'api/ride_log_ride#create'
-
     get '/subscriptions', to: 'webhook_subscription#index'
-
-    get '/api/hook', to: 'webhook_subscription#index'
-    post '/api/hook', to: 'api/hook#subscribe'
-    delete '/api/hook', to: 'api/hook#unsubscribe'
 
 end
