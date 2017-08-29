68957a9fadec957646da3f0e2b7fe7fa6192a3eb
Greg Shailes <greg.shailes@gmail.com>
2017-08-29 10:34:27 +0100

    Show full year in diary entries

--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,7 +1,7 @@
 class DiaryEntry < ApplicationRecord
 
   def entry_date
-    created_at.strftime("#{created_at.day.ordinalize} %B %y")
+    created_at.strftime("#{created_at.day.ordinalize} %B %Y")
   end
 
 end
