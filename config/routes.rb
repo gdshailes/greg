ca5105a16cea4f81d91dc433f5b287951a1f7eb4
Greg Shailes <greg.shailes@gmail.com>
2017-09-04 15:00:45 +0100

    Fix devise for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
-    root 'greghome#index'
 
-    get 'greghome', to: 'greghome#greghome'
-    get 'boriscam', to: 'boriscam#index'
-    post 'boriscam/upload', to: 'boriscam#upload'
+  root to: "greghome#index"
+  get 'greghome', to: 'greghome#greghome'
+  get 'boriscam', to: 'boriscam#index'
+  post 'boriscam/upload', to: 'boriscam#upload'
 
-    get 'ride_log', to: 'ride_log#index'
+  get 'ride_log', to: 'ride_log#index'
 
-    resources :ride_log_bike
-    resources :ride_log_ride
+  resources :ride_log_bike
+  resources :ride_log_ride
 
-    resources :diary_entry
+  resources :diary_entry
 
 end
