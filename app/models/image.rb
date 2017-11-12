0152ab5eb31a4c093ecb0348fbe32cd72272a78d
Greg Shailes <greg.shailes@gmail.com>
2017-11-12 21:25:24 +0000

    resize images on upload

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,8 @@ class Image < ActiveRecord::Base
   def uploaded_file(incoming_file)
     self.filename = incoming_file.original_filename
     self.filetype = incoming_file.content_type
-    self.filedata = incoming_file.read
+    self.filedata = MiniMagick::Image.read(incoming_file.read).resize('500x2000').to_blob
+
   end
 
 end
