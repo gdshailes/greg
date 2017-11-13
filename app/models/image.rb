b9980331eb5f5b8d9b9345450f8d86fd718bcdcc
Greg Shailes <greg.shailes@gmail.com>
2017-11-13 10:42:44 +0000

    update image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,9 @@
 class Image < ActiveRecord::Base
 
   def uploaded_file(incoming_file, size_width, size_height)
-    # self.filename = incoming_file.original_filename
-    # self.filetype = incoming_file.content_type
+    self.filename = incoming_file.original_filename
+    self.filetype = incoming_file.content_type
     self.filedata = MiniMagick::Image.read(incoming_file.read).resize("#{size_width}x#{size_height}").to_blob
-
   end
 
 end
