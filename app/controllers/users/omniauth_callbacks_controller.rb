a5efbb91ea8ddd6905d118c74d8ac65662fe6281
Greg Shailes <greg.shailes@gmail.com>
2017-11-16 15:54:39 +0000

    whoops

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    existing_user = User.find_by(email: @user.email) || @user
+    existing_user = User.find_by(email: @user.email)
+    if existing_user
+      @user = existing_user
+    end
     sign_in_and_redirect @user, :event => :authentication
     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
   end
