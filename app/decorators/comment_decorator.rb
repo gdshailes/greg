d53de2114b3c6513187b85d280acfea69831ab31
Greg Shailes <greg.shailes@gmail.com>
2017-11-24 12:31:21 +0000

    slight tweak to ratrace comment recency description

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -10,22 +10,22 @@ class ::CommentDecorator
   end
 
   def commenter_name
-    comment.user.first_name || comment.user.name || comment.user.email
+    user.display_name
   end
 
   def humanized_recency
     case
     when created_at > 10.minutes.ago
-      "Just now"
+      "just now"
     when created_at > 1.hour.ago
       "within the last hour"
     when created_at > 3.hours.ago
-      "Two or three hours ago"
+      "ywo or three hours ago"
     when created_at > Time.current.beginning_of_day
-      "Earlier today"
+      "earlier today"
     when created_at > (Time.current - 1.day).beginning_of_day
-      "Yesterday"
-    when created_at > (Time.current - 7.days).beginning_of_day
+      "yesterday"
+    when created_at > (Time.current - 5.days).beginning_of_day
       created_at.strftime("%A")
     when created_at > (Time.current.beginning_of_year)
       created_at.strftime("#{created_at.day.ordinalize} %B")
