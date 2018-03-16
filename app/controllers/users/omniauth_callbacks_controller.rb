99273fdb64c01558ba3139855dac1f35603005e4
Greg Shailes <greg.shailes@gmail.com>
2018-03-16 11:30:04 +0000

    Fix bug with omniauth creating duplicate user emails

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,6 +16,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def get_user_from_omniauth(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    existing_user = User.find_by(email: @user.email)
+    if existing_user
+      @user = existing_user
+    end
     sign_in_and_redirect @user, :event => :authentication
     set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
   end
