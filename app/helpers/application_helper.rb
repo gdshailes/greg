89c927cb7938a1fab212769532f58112f89f9d9d
Greg Shailes <greg.shailes@gmail.com>
2018-07-24 16:57:19 +0100

    Format block code nicely

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,13 @@
 module ApplicationHelper
-  # Convert markdown to HTML
+
   def markdown(text)
-    options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
-    }
-
-    extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
-    }
-
-    renderer    = Redcarpet::Render::HTML.new(options)
-    @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
+    if @markdown.nil?
+      options = {:hard_wrap => true}
+      extensions = { :fenced_code_blocks => true }
+      renderer = Redcarpet::Render::HTML.new(options)
+      @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
+    end
     @markdown.render(text).html_safe
   end
+
 end
