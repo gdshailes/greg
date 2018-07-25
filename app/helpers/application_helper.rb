d8e4177e1c001ece68585d528300cd905b357ed8
Greg Shailes <greg.shailes@gmail.com>
2018-07-25 09:39:23 +0100

    add classes to inline and block code generated from markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,22 @@
 module ApplicationHelper
 
+  class DiaryHTMLRenderer < Redcarpet::Render::HTML
+
+    def codespan(code)
+      "<code class='inline'>#{code}</code>"
+    end
+
+    def block_code(code, language)
+      "<pre class='block-code #{language}'>#{code}</pre>"
+    end
+
+  end
+
   def markdown(text)
     if @markdown.nil?
       options = {:hard_wrap => true}
       extensions = { :fenced_code_blocks => true }
-      renderer = Redcarpet::Render::HTML.new(options)
+      renderer = DiaryHTMLRenderer.new(options)
       @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
     end
     @markdown.render(text).html_safe
